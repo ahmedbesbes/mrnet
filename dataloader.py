dde6d1ada11acc7ee13875f52012d4dca1d0a33c
Ahmed Besbes <ahmedbesbes@mbp-de-ahmed.corp.capgemini.com>
2019-04-17 18:31:23 +0200

    adding data augmentation

--- dataloader.py
+++ dataloader.py
@@ -5,18 +5,24 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.utils.data as data
+from torchvision import transforms
+from torchsample.transforms import RandomRotate, RandomTranslate
+
 
 INPUT_DIM = 224
 MAX_PIXEL_VAL = 255
 MEAN = 58.09
 STDDEV = 49.73
 
+RandomTranslate
+
 
 class Dataset(data.Dataset):
-    def __init__(self, task, plane, train=True):
+    def __init__(self, task, plane, train=True, transform=True):
         super().__init__()
         self.task = task
         self.plane = plane
+        self.transform = transform
         if train:
             self.path = './data/train/{0}/'.format(plane)
             self.path_files = [self.path + p for p in os.listdir(self.path)]
@@ -27,6 +33,12 @@ class Dataset(data.Dataset):
             self.path_files = [self.path + p for p in os.listdir(self.path)]
             self.labels = pd.read_csv(
                 './train/valid-{0}.csv'.format(task), header=None)[1]
+        self.data_transform = transforms.Compose([
+            transforms.RandomRotation(25),
+            transforms.RandomAffine(0, translate=(0.11, 0.11)),
+            transforms.RandomHorizontalFlip()
+        ])
+        
 
     def __getitem__(self, index):
         array = np.load(self.path_files[index])
@@ -48,5 +60,8 @@ class Dataset(data.Dataset):
 
         array = torch.FloatTensor(array)
         label = torch.FloatTensor(label)
+
+        if self.transform:
+            array = self.data_transform(array)
 
         return array, label
