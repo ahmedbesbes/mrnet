d1354bb1f954a1de1a7daf4a668db06434c1df06
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-04-29 22:53:40 +0100

    fixing data augmentation

--- dataloader.py
+++ dataloader.py
@@ -7,22 +7,21 @@ import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 import torch.utils.data as data
 from torchvision import transforms
-from torchsample.transforms import RandomRotate, RandomTranslate
+from torchsample.transforms import RandomRotate, RandomTranslate, RandomFlip, ToTensor, Compose, RandomAffine
 
 
 INPUT_DIM = 224
 MAX_PIXEL_VAL = 255
 MEAN = 58.09
 STDDEV = 49.73
- 
+
 
 class MRDataset(data.Dataset):
-    def __init__(self, data_path, task, plane, train=True, weights=None, augment=True):
+    def __init__(self, data_path, task, plane, train=True, weights=None, transform=None):
         super().__init__()
         self.task = task
         self.plane = plane
         self.data_path = data_path
-        self.augment = augment
         if train:
             self.path = self.data_path + 'train/{0}/'.format(plane)
             self.path_files = [self.path + p for p in os.listdir(self.path)]
@@ -34,9 +33,7 @@ class MRDataset(data.Dataset):
             self.labels = pd.read_csv(
                 self.data_path + 'valid-{0}.csv'.format(task), header=None)[1]
 
-        self.to_pil = transforms.ToPILImage()
-        self.to_tensor = transforms.ToTensor()
-
+        self.transform = transform
         if weights is None:
             pos = np.sum(self.labels)
             neg = len(self.labels) - pos
@@ -47,28 +44,10 @@ class MRDataset(data.Dataset):
     def __len__(self):
         return len(self.path_files)
 
-    def transform(self, mri):
-        s = mri.shape[0]
-        degrees = np.random.randint(-25, 25)
-        pixels = np.random.randint(-25, 25)
-        proba_flip = np.random.random()
-        processed_slides = []
-        for i in range(s):
-            slide = mri[i]
-            slide = self.to_pil(slide)
-            slide = TF.affine(slide, degrees, [pixels, pixels], 1, 0)
-            if proba_flip > 0.5:
-                slide = TF.hflip(slide)
-            slide = self.to_tensor(slide)
-            slide = slide.unsqueeze(0)
-            processed_slides.append(slide)
-        processed_slides = torch.cat(processed_slides, 0)
-        # processed_slides = processed_slides.unsqueeze(0)
-        return processed_slides
-
     def __getitem__(self, index):
         array = np.load(self.path_files[index])
         label = self.labels[index]
+        label = torch.FloatTensor([label])
 
         # crop middle
         pad = int((array.shape[2] - INPUT_DIM)/2)
@@ -81,13 +60,7 @@ class MRDataset(data.Dataset):
         # normalize
         array = (array - MEAN) / STDDEV
 
-        # convert to RGB
-        array = np.stack((array,) * 3, axis=1)
-
-        array = torch.FloatTensor(array)
-        label = torch.FloatTensor([label])
-
-        if self.augment:
+        if self.transform:
             array = self.transform(array)
 
         if label.item() == 1:
