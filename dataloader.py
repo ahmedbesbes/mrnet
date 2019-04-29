ba7f8cbc1e82b45a8886b36f3f2c7772c15bb7b4
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-04-29 12:20:23 +0100

    add imagenet preprocessing and add augment attribute

--- dataloader.py
+++ dataloader.py
@@ -17,11 +17,12 @@ STDDEV = 49.73
 
 
 class MRDataset(data.Dataset):
-    def __init__(self, data_path, task, plane, train=True, weights=None):
+    def __init__(self, data_path, task, plane, train=True, weights=None, augment=True):
         super().__init__()
         self.task = task
         self.plane = plane
         self.data_path = data_path
+        self.augment = augment
         if train:
             self.path = self.data_path + 'train/{0}/'.format(plane)
             self.path_files = [self.path + p for p in os.listdir(self.path)]
@@ -32,7 +33,7 @@ class MRDataset(data.Dataset):
             self.path_files = [self.path + p for p in os.listdir(self.path)]
             self.labels = pd.read_csv(
                 self.data_path + 'valid-{0}.csv'.format(task), header=None)[1]
-        
+
         self.to_pil = transforms.ToPILImage()
         self.to_tensor = transforms.ToTensor()
 
@@ -68,16 +69,16 @@ class MRDataset(data.Dataset):
         array = np.load(self.path_files[index])
         label = self.labels[index]
 
-        # # crop middle
-        # pad = int((array.shape[2] - INPUT_DIM)/2)
-        # array = array[:, pad:-pad, pad:-pad]
+        # crop middle
+        pad = int((array.shape[2] - INPUT_DIM)/2)
+        array = array[:, pad:-pad, pad:-pad]
 
-        # # standardize
-        # array = (array - np.min(array)) / \
-        #     (np.max(array) - np.min(array)) * MAX_PIXEL_VAL
+        # standardize
+        array = (array - np.min(array)) / \
+            (np.max(array) - np.min(array)) * MAX_PIXEL_VAL
 
-        # # normalize
-        # array = (array - MEAN) / STDDEV
+        # normalize
+        array = (array - MEAN) / STDDEV
 
         # convert to RGB
         array = np.stack((array,) * 3, axis=1)
@@ -85,7 +86,8 @@ class MRDataset(data.Dataset):
         array = torch.FloatTensor(array)
         label = torch.FloatTensor([label])
 
-        array = self.transform(array)
+        if self.augment:
+            array = self.transform(array)
 
         if label.item() == 1:
             weight = np.array([self.weights[1]])
