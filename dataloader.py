c46d43937c6a7dc3ec3bc69f0c5f082e5890350f
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-05-04 18:06:44 +0200

    fixing data loader constructor to add train attribute

--- dataloader.py
+++ dataloader.py
@@ -17,22 +17,26 @@ STDDEV = 49.73
 
 
 class MRDataset(data.Dataset):
-    def __init__(self, root_dir, task, plane, indexes, weights=None, transform=None, train=True):
+    def __init__(self, root_dir, task, plane, train=True, transform=None, weights=None):
         super().__init__()
         self.task = task
         self.plane = plane
         self.root_dir = root_dir
-        if train:
+        self.train = train
+        if self.train:
             self.folder_path = self.root_dir + 'train/{0}/'.format(plane)
-            self.records = pd.read_csv(self.root_dir  + 'train-{0}.csv'.format(task), header=None, names=['id', 'label'])
+            self.records = pd.read_csv(
+                self.root_dir + 'train-{0}.csv'.format(task), header=None, names=['id', 'label'])
         else:
+            transform = None
             self.folder_path = self.root_dir + 'valid/{0}/'.format(plane)
-            self.records = pd.read_csv(self.root_dir  + 'valid-{0}.csv'.format(task), header=None, names=['id', 'label'])
-        
-        self.records['id'] = self.records['id'].map(lambda i: '0' * (4 - len(str(i))) + str(i))
-        self.records = self.records.loc[indexes]
+            self.records = pd.read_csv(
+                self.root_dir + 'valid-{0}.csv'.format(task), header=None, names=['id', 'label'])
 
-        self.paths = [self.folder_path + filename + '.npy' for filename in self.records['id'].tolist()]
+        self.records['id'] = self.records['id'].map(
+            lambda i: '0' * (4 - len(str(i))) + str(i))
+        self.paths = [self.folder_path + filename +
+                      '.npy' for filename in self.records['id'].tolist()]
         self.labels = self.records['label'].tolist()
 
         self.transform = transform
@@ -68,11 +72,13 @@ class MRDataset(data.Dataset):
             array = np.stack((array,)*3, axis=1)
             array = torch.FloatTensor(array)
 
-        if label.item() == 1:
-            weight = np.array([self.weights[1]])
-            weight = torch.FloatTensor(weight)
+        if self.train:
+            if label.item() == 1:
+                weight = np.array([self.weights[1]])
+                weight = torch.FloatTensor(weight)
+            else:
+                weight = np.array([self.weights[0]])
+                weight = torch.FloatTensor(weight)
+            return array, label, weight
         else:
-            weight = np.array([self.weights[0]])
-            weight = torch.FloatTensor(weight)
-
-        return array, label, weight
+            return array, label
