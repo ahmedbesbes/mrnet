6e33784f78d960daedcaf7ed9dc4e6da27bde584
Ahmed Besbes <ahmed.besbes@hotmail.com>
2019-05-05 18:51:24 +0200

    packaging the code in train.py

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import argparse
 import numpy as np
 from tqdm import tqdm
@@ -6,45 +7,203 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
+from torchsample.transforms import RandomRotate, RandomTranslate, RandomFlip, ToTensor, Compose, RandomAffine
+from torchvision import transforms
+from tensorboardX import SummaryWriter
 
 from dataloader import MRDataset
 from model import MRNet
- 
 
-def train(model, trainloader, epoch, criterion, optimizer, num_epochs):
+
+def train_model(model, train_loader, epoch, num_epochs, optimizer, writer, log_every=100):
     _ = model.train()
-    train_losses = []
-    for i, (image, label) in enumerate(trainloader): 
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+    y_preds = []
+    y_trues = []
+    losses = []
+
+    for i, (image, label, weight) in enumerate(train_loader):
         optimizer.zero_grad()
-        prediction = model.forward(image)
-        prediction = torch.tensor(prediction)
-        loss = criterion(label[0], prediction[0])
+
+        if torch.cuda.is_available():
+            image = image.cuda()
+            label = label.cuda()
+            weight = weight.cuda()
+
+        prediction = model.forward(image.float())
+
+        y_pred = torch.sigmoid(prediction).item()
+        y_true = int(label.item())
+
+        y_preds.append(y_pred)
+        y_trues.append(y_true)
+
+        try:
+            auc = metrics.roc_auc_score(y_trues, y_preds)
+        except:
+            auc = 0.5
+
+        loss = F.binary_cross_entropy_with_logits(
+            prediction[0], label[0], weight=weight[0])
+
         loss.backward()
         optimizer.step()
-        loss = loss.item()
-        train_losses.append(loss)
-        print('[Epoch: {0} / {1} |Â Batch: {2} / {3} ]| train loss {4}'.format(
-            epoch,
-            num_epochs,
-            i,
-            len(trainloader),
-            np.mean(train_losses)
-        ))
+
+        loss_value = loss.item()
+        losses.append(loss_value)
+
+        writer.add_scalar('Train/Loss', loss_value,
+                          epoch * len(train_loader) + i)
+        writer.add_scalar('Train/AUC', auc, epoch * len(train_loader) + i)
+
+        if (i % log_every == 0) & (i > 0):
+            print('''[Epoch: {0} / {1} |Single batch number : {2} / {3} ]| avg train loss {4} | train auc : {5}'''.
+                  format(
+                      epoch + 1,
+                      num_epochs,
+                      i,
+                      len(train_loader),
+                      np.round(np.mean(losses), 4),
+                      np.round(auc, 4)
+                  )
+                  )
+
+    writer.add_scalar('Train/AUC_epoch', auc, epoch + i)
+
+    train_loss_epoch = np.round(np.mean(losses), 4)
+    train_auc_epoch = np.round(auc, 4)
+    return train_loss_epoch, train_auc_epoch
+
+
+def evaluate_model(model, val_loader, epoch, num_epochs, writer, log_every=20):
+    _ = model.eval()
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+    y_trues = []
+    y_preds = []
+    losses = []
+
+    for i, (image, label, weight) in enumerate(val_loader):
+
+        if torch.cuda.is_available():
+            image = image.cuda()
+            label = label.cuda()
+            weight = weight.cuda()
+
+        prediction = model.forward(image.float())
+
+        y_pred = torch.sigmoid(prediction).item()
+        y_true = int(label.item())
+
+        y_preds.append(y_pred)
+        y_trues.append(y_true)
+
+        try:
+            auc = metrics.roc_auc_score(y_trues, y_preds)
+        except:
+            auc = 0.5
+
+        loss = F.binary_cross_entropy_with_logits(
+            prediction[0], label[0], weight=weight[0])
+
+        loss_value = loss.item()
+        losses.append(loss_value)
+
+        writer.add_scalar('Val/Loss', loss_value, epoch * len(val_loader) + i)
+        writer.add_scalar('Val/AUC', auc, epoch * len(val_loader) + i)
+
+        if (i % log_every == 0) & (i > 0):
+            print('''[Epoch: {0} / {1} |Single batch number : {2} / {3} ] | avg val loss {4} | val auc : {5}'''.
+                  format(
+                      epoch + 1,
+                      num_epochs,
+                      i,
+                      len(val_loader),
+                      np.round(np.mean(losses), 4),
+                      np.round(auc, 4)
+                  )
+                  )
+
+    writer.add_scalar('Val/AUC_epoch', auc, epoch + i)
+
+    val_loss_epoch = np.round(np.mean(losses), 4)
+    val_auc_epoch = np.round(auc, 4)
+    return val_loss_epoch, val_auc_epoch
 
 
 def run(args):
-    mrnet = MRNet()
-    criterion = nn.BCELoss()
-    optimizer = optim.Adam(mrnet.parameters())
+    now = datetime.now()
+    logdir = "./logs/" + now.strftime("%Y%m%d-%H%M%S") + "/"
+    os.makedirs(logdir)
 
-    trainset = MRDataset('./data/', args.task, args.plane, transform=bool(args.augment))
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=1, shuffle=True, num_workers=8)
+    writer = SummaryWriter(logdir)
 
-    for epoch in tqdm(range(args.epochs)):
-        train(mrnet, trainloader, epoch, criterion, optimizer, args.epochs)
+    augmentor = Compose([
+        transforms.Lambda(lambda x: torch.Tensor(x)),
+        RandomTranslate([0.11, 0.11]),
+        RandomRotate(25),
+        RandomFlip(),
+        transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1).permute(1, 0, 2, 3)),
+    ])
 
-    print('finshed training')
+    train_dataset = MRDataset('./data/', args.task,
+                              args.plane, transform=augmentor, train=True)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=1, shuffle=True, num_workers=11, drop_last=False)
+
+    validation_dataset = MRDataset(
+        './data/', args.task, args.plane, train=False)
+    validation_loader = torch.utils.data.DataLoader(
+        validation_dataset, batch_size=1, shuffle=-True, num_workers=11, drop_last=False)
+
+    mrnet = model.MRNet()
+    mrnet = mrnet.cuda()
+
+    optimizer = optim.Adam(mrnet.parameters(), lr=1e-5, weight_decay=0.01)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, patience=3, factor=.3, threshold=1e-4, verbose=True)
+
+    best_val_loss = float('inf')
+    best_val_auc = float(0)
+
+    num_epochs = args.epochs
+    iteration_change_loss = 0
+    patience = 4
+
+    for epoch in range(num_epochs):
+
+        train_loss, train_auc = train_model(
+            mrnet, train_loader, epoch, num_epochs, optimizer, writer)
+        val_loss, val_auc = evaluate_model(
+            mrnet, validation_loader, epoch, num_epochs, writer)
+
+        print('train loss : {} |train auc {} | val loss {} | val auc {}'.format(train_loss,
+                                                                                 train_auc,
+                                                                                 val_loss,
+                                                                                 val_auc))
+        scheduler.step(val_loss)
+        iteration_change_loss += 1
+
+        print('-' * 30)
+
+        if val_auc > best_val_auc:
+            best_val_auc = val_auc
+            file_name = f'val_auc{val_auc:0.4f}_train_auc{train_auc:0.4f}_epoch{epoch+1}.pth'
+            torch.save(mrnet, '../models/{0}'.format(file_name))
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            iteration_change_loss = 0
+
+        if iteration_change_loss == patience:
+            print('Early stopping after {0} iterations without the decrease of the val loss'.
+                  format(iteration_change_loss))
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -54,7 +213,7 @@ def parse_arguments():
                         choices=['saggital', 'coronal', 'axial'])
     parser.add_argument('--augment', type=int, choices=[0, 1], default=1)
     parser.add_argument('--epochs', type=int, default=50)
-    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--lr', type=float, default=1e-5)
     args = parser.parse_args()
     return args
 
